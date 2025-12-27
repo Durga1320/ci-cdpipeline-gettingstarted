ce6346fd79b117b0f4bcceda3b623eb96b73bb90
Durga1320 <durgaprasadkatakamsetti13@gmail.com>
2025-12-28 01:13:57 +0530

    added multiple function

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import add
+from src.main import add,multiply
 
 def test_add():
     assert add(2, 3) == 5
@@ -6,3 +6,10 @@ def test_add():
     assert add(0, 0) == 0
     assert add(100, 200) == 300
     print('---all passed')
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+    assert multiply(-1, 1) == -1
+    assert multiply(0, 0) == 0
+    assert multiply(100, 200) == 20000
+    print('---all multiply passed')
